beac3d1cf8517edf425c4fb3f5822f2f1bd49ad3
Mousumi Dsa <58068282+Mousumi84@users.noreply.github.com>
2024-05-03 01:09:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
+
+
+let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 
 const App = () => {
+const [searchFruits,setSearchFruits]=useState(" ");
+  
   return (
     <div>
         {/* Do not remove the main div */}
+
+    <h1>Search item</h1>
+    <input type="search" value={searchFruits} onChange={(e)=>setSearchFruits(e.target.value)} />
+    <ul>
+    
     </div>
   )
 }
